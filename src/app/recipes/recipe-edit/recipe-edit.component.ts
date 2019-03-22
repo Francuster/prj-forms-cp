71df09682c431dd223ea57d193e611b85d5c40ca
Franco Disabato <francuster1@gmail.com>
2019-03-22 17:14:27 -0300

    Syncing HTML with the Form

--- src/app/recipes/recipe-edit/recipe-edit.component.ts
+++ src/app/recipes/recipe-edit/recipe-edit.component.ts
@@ -27,6 +27,11 @@ export class RecipeEditComponent implements OnInit {
       );
   }
 
+  onSubmit(){
+    console.log(this.recipeForm);
+    
+  }
+
   private initForm(){
     let recipeName = '';
     let recipeImagePath = '';
